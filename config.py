bf701f7811d89913c1e6b8771c3a5c0fce2738a5
Seu Nome <David.nascimento@sou.inteli.edu.br>
2025-03-05 16:53:19 -0300

    feat: new config

--- config.py
+++ config.py
@@ -15,15 +15,15 @@ def get_model_class(module_name, class_name):
 SEGMENT_CONFIG = {
     "X5": {
         "api_key": os.getenv("OPENAI_API_KEY"),
-        "db_url": os.getenv("DATABASE_URL"),
+        "db_url": os.getenv("DATABASE_URL_X5"),
         "models": {
-            "GIX5": lambda api_key: get_model_class("gix5", "GIX5Agent")(api_key),
-            "LEGAL": lambda api_key: get_model_class("legal", "LegalAI")(api_key),
-            "BNCC": lambda api_key: get_model_class("bncc", "BNCCAgent")(api_key),
-            "PEE": lambda api_key: get_model_class("pee", "PEEAgent")(api_key),
+            "GIX5_X5_OMINI": lambda api_key: get_model_class("gix5", "GIX5Agent")(api_key),
+            "JURIDICO_X5": lambda api_key: get_model_class("legal", "LegalAI")(api_key),
+            "AGENTE_X5_PROGRAMA_EMPRESA_EXPONENCIAL": lambda api_key: get_model_class("pee", "PEEAgent")(api_key),
+            # "FINANCEIRO_X5": lambda api_key: get_model_class("financeiro", "Financeiro")(api_key),
         }
     },
-    "ESCOLAS": {
+    "DARTHA": {
         "api_key": os.getenv("OPENAI_API_KEY"),
         "db_url": os.getenv("DATABASE_URL"),  # 🔥 Verifique se isso não está retornando None
         "models": {
